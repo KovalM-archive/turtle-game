1bc78c9767943c869edc5238c9b2466de6f37754
KovalM <m.koval@mail.by>
2015-07-18 18:44:22 +0300

    turtle go

---
+++ js/turtle.js
@@ -0,0 +1,35 @@
+var speedTurtle;
+var svansDown;
+$(document).ready(function(currentSpeed) {
+    	eval($.turtle());
+    	speedTurtle=currentSpeed;
+    	goHome();
+    	pd();
+    	svansDown=true;
+    	pen('red');
+    });
+
+runProgramm = function(){
+	fd(100);
+}
+
+goHome = function(){
+	cg();
+	home();
+	speed(Infinity);
+	pu();
+    moveto(-280,-20	);
+    if (svansDown) pd();
+   	speed(speedTurtle);
+}
+
+doPause = function(){
+	sleep(1000)
+}
+
+changeSpeed = function(newSpeed){
+	speedTurtle = newSpeed;
+	speed(speedTurtle);
+	result = document.getElementById("currentspeed");
+	result.value = speedTurtle;
+}
