72884f934bb83ca748099f9d533950f917fa9c43
KovalM <m.koval@mail.by>
2015-07-19 23:43:25 +0300

    full circle

--- js/turtle.js
+++ js/turtle.js
@@ -9,13 +9,12 @@ $(document).ready(function() {
     	pen('red');
     });
 
+var commands,numberCommands;
 runProgramm = function(){
 	var programm = document.getElementById("programm");
-	var commands = programm.childNodes;
-	var n = commands.length;
-	for (var i=0; i<n; i++){
-		
-	}
+	commands = programm.childNodes;
+	numberCommands = commands.length;
+	doProgram(0,1);
 }
 
 goHome = function(){
@@ -39,3 +38,48 @@ changeSpeed = function(newSpeed){
 	result.value = speedTurtle;
 }
 
+doProgram = function(start,n){
+	var i=0;
+	while (i<n){
+		var j = start;
+		ans = 0;
+		while (true){
+			if (j>=numberCommands) break;
+			var commandType = commands[j].getAttribute('data-type-command');
+			switch(commandType){
+			case "fd-command":
+				fd(getValue(commands[j]));
+				break;
+			case "rt-command":
+				rt(getValue(commands[j]));
+				break;
+			case "lt-command":
+				lt(getValue(commands[j]));
+				break;
+			case "pu-command":
+				pu();
+				break;
+			case "pd-command":
+				pd();
+				break;
+			case "repeat-command":
+				j=doProgram(j+1,getValue(commands[j]));
+				break;
+			case "end-circle-command":
+				ans = j;
+				break;
+			default:
+				break;
+			}
+			if (ans!=0) break;
+			j++;
+		}
+		i++;
+	}
+	return ans;
+}
+
+getValue = function(element){
+	var elements = element.getElementsByTagName('input');
+	return elements[0].value;
+}
